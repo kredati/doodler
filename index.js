8800c600e3661c2653b58eed748f7a94bdc70c28
Scott Richmond <s.richmond@utoronto.ca>
2017-01-17 11:46:50 -0500

    Change name from Sketcher to Doodler
    I figured it was confusing.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict'
 
-// This is an array: it can hold multiple things.
-// In principle, we can have multiple sketchers at once
-let sketchers = []
+// This is an array: it can hold multiple doodles.
+// In principle, we can have multiple doodlers at once.
+// Where would we change the number of doodlers?
+let doodles = []
 
 // setup() is called once at the beginning of the sketch
 var setup = () => {
-  createCanvas(600, 600)
+  createCanvas(800, 600)
   background(230, 230, 230)
-  1..times(() => sketchers.push(new Sketcher()))
+  1..times(() => doodles.push(new Doodler()))
 }
 
 // draw() composes each frame of the animation
 // It is called approx. 60 times per second
 var draw = () => {
-  sketchers.forEach(sketcher => sketcher.sketch())
+  doodles.forEach(doodler => doodler.doodle())
 }
--- sketcher.js
+++ sketcher.js
@@ -1,7 +1,7 @@
 'use strict'
 
-// Defines a Sketcher object that doodles on the canvas
-class Sketcher {
+// Defines a Doodler object that doodles on the canvas
+class Doodler {
 
   // The constructor is called when the object is created
   constructor() {
@@ -10,20 +10,21 @@ class Sketcher {
       this.noiseResolution = 0.01
   }
 
-  // Sketch does all the delegtaion, to update and display
-  sketch() {
+  // Doodle does all the delegtaion, to update and display
+  doodle() {
     this.update()
     this.display()
   }
 
-  // Display draws a sketcher
+  // Display draws the current frame of the doodler
   display() {
     fill(0, 0, 0, 30)
     noStroke()
     ellipse(this.position.x, this.position.y, 4, 4)
   }
 
-  // And update moves its position. What's doing the heavy lifting here?
+  // And update moves its position for the current frame.
+  // What's doing the heavy lifting here?
   update() {
     this.position.x = map(noise(this.noise.x), 0, 1, 0, width)
     this.position.y = map(noise(this.noise.y), 0, 1, 0, height)
